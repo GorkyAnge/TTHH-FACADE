f1d661c780053f932566f3232fc5605309183c15
Sebastian Quito <josue.quito@udla.edu.ec>
2023-12-16 00:24:37 -0500

    added validations for UsuarioDto

--- ModeladoTTHH/Models/UsuarioDto.cs
+++ ModeladoTTHH/Models/UsuarioDto.cs
@@ -1,7 +1,14 @@
-﻿namespace ModeladoTTHH.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModeladoTTHH.Models;
 
 public class UsuarioDto
 {
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Usuario es requerido")]
+    [MinLength(4, ErrorMessage = "El campo Usuario debe tener 4 caracteres")]
+    [RegularExpression("^[0-9]+$", ErrorMessage = "El campo Usuario debe contener solo números")]
     public string Usuario { get; set; }
+    
+    [Required(AllowEmptyStrings = false, ErrorMessage = "El campo Contraseña es requerido")]
     public string Password { get; set; }
 }
